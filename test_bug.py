8c956878dc2370c007fa61913cb30cafc7c4b321
Ranjana H <ranjanah0521@gmail.com>
2026-05-18 18:30:45 +0530

    Update test_bug.py

--- test_bug.py
+++ test_bug.py
@@ -1,1 +1,8 @@
-
+def fetch_data(query_string):
+    # DANGEROUS: eval on raw input
+    result = eval(query_string)
+    
+    # INEFFICIENT: generating massive list in memory just to count
+    items = [i for i in range(100000000)]
+    
+    return len(items) + result
